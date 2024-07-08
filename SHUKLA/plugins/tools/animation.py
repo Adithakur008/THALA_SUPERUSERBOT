b22f7c48035a9265c4c96da76d24683bd65bdc06
Adithakur008 <Rajaryan231207@gmail.com>
2024-07-08 07:22:39 +0530

    Update animation.py

--- SHUKLA/plugins/tools/animation.py
+++ SHUKLA/plugins/tools/animation.py
@@ -9,8 +9,8 @@ from pyrogram.types import *
 from pyrogram.types import Message
 
 from ... import *
-from THALA.modules.FAMILY.basic import edit_or_reply, get_text
-from THALA.modules.FAMILY.constants import MEMES
+from SHUKLA.modules.SHASHANK.basic import edit_or_reply, get_text
+from SHUKLA.modules.SHASHANK.constants import MEMES
 
 
 
