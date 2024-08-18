d4ffbf9ff86c7d6f68588976bd6676ab65fe0de8
DESTROYER <Rajaryan231207@gmail.com>
2024-08-18 10:29:29 +0530

    Update __init__.py

--- SHUKLA/plugins/__init__.py
+++ SHUKLA/plugins/__init__.py
@@ -6,7 +6,7 @@ def __list_all_plugins():
     work_dir = dirname(__file__)
     mod_paths = glob.glob(work_dir + "/*/*.py")
 
-    all_plugins = []
+    all_plugins = [
         (((f.replace(work_dir, "")).replace("/", "."))[:-3])
         for f in mod_paths
         if isfile(f)
