de10f5d438d700c773a12dec2f1fc5ee97282857
DESTROYER <Rajaryan231207@gmail.com>
2024-08-18 10:00:01 +0530

    Update __init__.py

--- SHUKLA/__init__.py
+++ SHUKLA/__init__.py
@@ -5,7 +5,7 @@ from aiohttp import ClientSession
 __version__ = "v2.1.0"
 
 spam_chats = []
-aiosession = ClientSession()
+Aiosession=ClientSession()
 SUDO_USER = SUDO_USERS
 OWNER_USERNAME = OWNER_USERNAME
 SUDO_USERS.append(OWNER_ID)
