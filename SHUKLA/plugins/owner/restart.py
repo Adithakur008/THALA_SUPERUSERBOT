c2f39f1b22cb2128607bf368f86bf7543b5ec7aa
DESTROYER <Rajaryan231207@gmail.com>
2024-08-28 16:03:58 +0530

    Update restart.py

--- SHUKLA/plugins/owner/restart.py
+++ SHUKLA/plugins/owner/restart.py
@@ -10,7 +10,7 @@ from ... import *
 async def restart(client: Client, message: Message):
     reply = await message.reply_text("**Restarting...**")
     await message.delete()
-    await reply.edit_text("Successfully Restarted THALAFAMILY PYTHON...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
+    await reply.edit_text("Successfully Restarted DESTROYER PYTHON...\n\n💞 Wait 1-2 minutes\nLoad plugins...</b>")
     os.system(f"kill -9 {os.getpid()} && python3 -m SHUKLA")
   
 
